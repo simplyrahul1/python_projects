3eb0008dc8379e8750424b8a05b47a1522920496
Rahul Sinha <simplyrahul@gmail.com>
2023-06-10 13:50:24 +0000

    update change limiter file

---
+++ change_limiter.py
@@ -0,0 +1,12 @@
+## Change limiter from "space" to "-"
+
+def change_limiter(my_sentence):
+    new_sentence = my_sentence.split(" ")
+    new_sentence = "-".join(new_sentence)
+    return (new_sentence)
+
+if __name__ == '__main__':
+    input_sentences = ["This is great day", "meeting is boring", "Awesome !!!"]
+    for input_element in input_sentences:
+        return_element = change_limiter(input_element)
+        print(return_element)
